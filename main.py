85460759709c2fc4c8410245d0f6abb76ef49a9c
ninjin09 <117459736+ninjin09@users.noreply.github.com>
2024-04-10 23:42:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,22 @@ class Transaction(BaseModel):
     product_category: str
     product_type: str
     size: str
+    
+@app.get("/transactions/")
+def read_items():
+    data = supabase.table("df").select("*").order("created_at", ascending=False).limit(10).execute()
+    if data.data:
+        return data.data
+    else:
+        raise HTTPException(status_code=404, detail="Transactions not found")
 
-
-@app.post("/transaction/")
+@app.post("/{transaction_id}/")
 def create_transaction(transaction: Transaction):
+    transaction.total_bill = transaction.calculate_total_bill()
     transaction.transaction_date = datetime.now().strftime('%Y-%m-%d')
     transaction.transaction_time = datetime.now().strftime('%H:%M:%S')
-    data = supabase.table("df").insert(transaction.dict()).execute()
-    if data["data"]:
-        return data["data"]
+    data = supabase_client.table("transactions").insert(transaction.dict()).execute()
+    if data.data:
+        return data.data
     else:
         raise HTTPException(status_code=400, detail="Transaction could not be created")
