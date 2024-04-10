1d4925f542a0ed4ec317836ef1cf1588fa31812e
ninjin09 <117459736+ninjin09@users.noreply.github.com>
2024-04-10 22:00:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from typing import Optional
+from supabase import create_client, Client
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-from fastapi import FastAPI
+# data from: https://www.kaggle.com/datasets/divu2001/coffee-shop-sales-analysis/data
+
+url: str = "https://lgrlgjkibvhjbybbmcwb.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxncmxnamtpYnZoamJ5YmJtY3diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2Mjc3NTYsImV4cCI6MjAyODIwMzc1Nn0.uX392Mdc1JCj0eeTQVcT5B3lVhnEJAtmWLngsd1yqrY"
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
